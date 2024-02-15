ccdd3c4911f61049f6dcd3d01076096086f01a6f
vladimirkostadinov <60099467+vladimirkostadinov@users.noreply.github.com>
2024-02-15 14:18:24 +0200

    Update 001_install_docker.sh

--- 001_install_docker.sh
+++ 001_install_docker.sh
@@ -6,7 +6,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker adminawx
+sudo usermod -aG docker $AWX_SVC_USER
 
 docker --version
 
