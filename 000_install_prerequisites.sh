061d3d11e25e3b530bcc96d2248bd3c998bc0f32
vladimirkostadinov <60099467+vladimirkostadinov@users.noreply.github.com>
2024-02-15 14:18:04 +0200

    Update 000_install_prerequisites.sh

--- 000_install_prerequisites.sh
+++ 000_install_prerequisites.sh
@@ -3,9 +3,9 @@ source ./_variables.sh
 echo " *** AWX Deployment - Install prerequisites *** "
 echo "Install prerequisites - ca-certificates curl gnupg libssl-dev git make "
 sudo apt-get -y install ca-certificates curl gnupg libssl-dev git make
-echo "Create New user - adminawx"
-sudo adduser adminawx --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "Create New user - $AWX_SVC_USER"
+sudo adduser $AWX_SVC_USER --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "adminawx:$AWX_SVC_PASS" | sudo chpasswd
-echo "Add user adminawx to sudo group"
-usermod -aG sudo adminawx
+echo "Add user $AWX_SVC_USER to sudo group"
+usermod -aG sudo $AWX_SVC_USER
 
