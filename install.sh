38fd3d25114b5bdfaaae4e2bec7dcb9d940cfff7
vladimirkostadinov <60099467+vladimirkostadinov@users.noreply.github.com>
2024-02-15 08:48:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ source ./common.sh
 ./000_install_prerequisites.sh
 ./001_install_docker.sh
 ./002_install_minikube.sh
-su admin ./003_start_minikube.sh
-su admin ./004_apply_awx_config.sh
-su admin ./005_monitor_creation.sh
+su awxadmin ./003_start_minikube.sh
+su awxadmin ./004_apply_awx_config.sh
+su awxadmin ./005_monitor_creation.sh
 ./006_create_minikube_service.sh
 ./007_create_start_up_script.sh
-su admin ./008_output_ui_password.sh
+su awxadmin ./008_output_ui_password.sh
