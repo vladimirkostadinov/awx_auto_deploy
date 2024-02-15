3e23872ced457f8b59f08aa7a318da5420c8fb0d
vladimirkostadinov <60099467+vladimirkostadinov@users.noreply.github.com>
2024-02-15 14:58:13 +0200

    Update _variables.sh

--- _variables.sh
+++ _variables.sh
@@ -1,5 +1,7 @@
 DOCKERHUB_USERNAME="your_docker_io_account"
 DOCKERHUB_PASSWORD="your_docker_pass_account"
 AWX_VERSION="2.9.0"
+AWX_CPU=4
+AWX_RAM=12
 AWX_SVC_USER="your_svc_account_username"
 AWX_SVC_PASS='your_svc_account_password'
