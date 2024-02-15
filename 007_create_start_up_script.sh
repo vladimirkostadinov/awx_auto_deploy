7ecc76699b8fa4784f60579dc61c11fb419a64c4
vladimirkostadinov <60099467+vladimirkostadinov@users.noreply.github.com>
2024-02-15 14:21:00 +0200

    Update 007_create_start_up_script.sh

--- 007_create_start_up_script.sh
+++ 007_create_start_up_script.sh
@@ -1,11 +1,11 @@
 echo " *** AWX Deployment - Create Start Up script ***"
 #this must be executed as root
-cat <<EOF > /home/adminawx/start_awx.sh
+cat <<EOF > /home/$AWX_SVC_USER/start_awx.sh
 retry=0
 while [ \$retry -lt 60 ]; do
     echo "Switch to AWX context" >> /home/$AWX_SVC_USER/start_awx.log 
 	kubectl config set-context --current --namespace=awx >> /home/$AWX_SVC_USER/start_awx.log
-    echo "Create Port forwarding ..." >> /home/adminawx/start_awx.log
+    echo "Create Port forwarding ..." >> /home/$AWX_SVC_USER/start_awx.log
 	kubectl port-forward service/awx-server-service --address 0.0.0.0 8080:80 >> /home/$AWX_SVC_USER/start_awx.log
 	sleep 20
 	retry=$((retry+1))
@@ -17,5 +17,5 @@ chmod +x /home/$AWX_SVC_USER/start_awx.sh
 
 echo "Create cron job"
 crontab -l > cron_file
-echo "@reboot su $AWX_SVC_USER /home/adminawx/start_awx.sh" >> cron_file
+echo "@reboot su $AWX_SVC_USER /home/$AWX_SVC_USER/start_awx.sh" >> cron_file
 crontab cron_file
