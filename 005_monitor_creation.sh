15cba89ce17be9a9d32f733fea64cad72040cac9
vladimirkostadinov <60099467+vladimirkostadinov@users.noreply.github.com>
2024-02-15 14:09:22 +0200

    Update 005_monitor_creation.sh

--- 005_monitor_creation.sh
+++ 005_monitor_creation.sh
@@ -1,7 +1,7 @@
 echo " *** AWX Deployment - Monitor deployment ***"
 retry=0
 kubectl config set-context --current --namespace=awx
-$logexpression="((?<=logger).*KubeAPIWarningLogger.*msg.*unknown field.*ansibleResult.*)"
+logexpression="((?<=logger).*KubeAPIWarningLogger.*msg.*unknown field.*ansibleResult.*)"
 while [[ ! $log_result=~$logexpression && $retry -lt 1000 ]]; do
     log_result=`kubectl logs --follow=false deployments/awx-operator-controller-manager -c awx-manager | tail -n 1`
     echo $log_result
