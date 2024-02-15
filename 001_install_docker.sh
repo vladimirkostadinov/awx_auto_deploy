8251ecddd75f88ea71fd8bc9d21faa341420fd9f
vladimirkostadinov <60099467+vladimirkostadinov@users.noreply.github.com>
2024-02-15 14:43:48 +0200

    Update 001_install_docker.sh

--- 001_install_docker.sh
+++ 001_install_docker.sh
@@ -1,12 +1,12 @@
 echo " *** AWX Deployment - Install Docker *** "
-
+source ./_variables.sh
 sudo install -m 0755 -d /etc/apt/keyrings 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker ${AWX_SVC_USER}
+sudo usermod -aG docker $AWX_SVC_USER
 
 docker --version
 
